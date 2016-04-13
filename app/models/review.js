3250c149e289edb9a1069fdcbf3801b2d7b7bcfe
David Remington <david.ian.remington@gmail.com>
2016-04-13 13:55:04 -0700

    add comments component

--- app/models/review.js
+++ app/models/review.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   username: DS.attr(),
   comment: DS.attr(),
   rating: DS.attr(),
+  date: DS.attr(),
   hike: DS.belongsTo('hike', {async: true})
 });
--- app/routes/hike.js
+++ app/routes/hike.js
@@ -3,5 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params){
     return this.store.findRecord('hike', params.hike_id);
+  },
+  addReview(params){
+    var newReview = this.store.createRecord('review', params);
+      var hike = params.hike;
+      hike.get('reviews').addObject(newReview);
+      newReview.save().then(function() {
+        return hike.save();
+      });
+      this.transitionTo('hike', params.hike);
   }
 });
