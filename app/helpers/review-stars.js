5f4e214eb8b16c10880689b4cb30db2ec826b385
David Remington <david.ian.remington@gmail.com>
2016-04-13 15:35:17 -0700

    implement bugfix for helper function

--- app/helpers/review-stars.js
+++ app/helpers/review-stars.js
@@ -1,13 +1,17 @@
 import Ember from 'ember';
 
 export function reviewStars(rating) {
+  var stars = "";
+  var i = 0;
   if(rating) {
     var parsed = parseInt(rating);
     var star = "{{fa-icon fa-star}}";
     for (i=0; i<parsed; i++) {
-      star
+      stars.concat(star);
     }
   }
+  console.log(rating);
+  return Ember.String.htmlSafe(stars);
 }
 
 export default Ember.Helper.helper(reviewStars);
