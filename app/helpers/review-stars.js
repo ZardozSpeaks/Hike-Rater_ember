52c0387940ed691ee80f98ed3e16e48bcff59663
David Remington <david.ian.remington@gmail.com>
2016-04-14 12:26:38 -0700

    add star icon to helper

--- app/helpers/review-stars.js
+++ app/helpers/review-stars.js
@@ -1,15 +1,12 @@
 import Ember from 'ember';
 
-export function reviewStars(params) {
-  console.log(params[0]);
-  let review = parseInt(params[0]),
-      stars="",
-      i = 0;
-  for (i=0; i<review; i++) {
-    stars.concat("{{fa-icon fa-star}}");
-  }
-  console.log(stars);
-  return Ember.String.htmlSafe(stars);
-}
-
 export default Ember.Helper.helper(reviewStars);
+
+export function reviewStars(params) {
+  let review = parseInt(params[0]),
+      stars=" - user's rating: ";
+  for (var i=0; i<review; i++) {
+    stars += ("★");
+  }
+  return stars;
+}
